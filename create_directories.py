365f20e44eb6ba9377f8579537af176481e7369c
Atharva Akshat <59068316+atharva-akshat@users.noreply.github.com>
2022-12-18 22:04:35 +0530

    Code to create directories for each day

---
+++ create_directories.py
@@ -0,0 +1,13 @@
+# Creating directories and readme files for each day
+# Run this code in an empty directory
+
+
+# import os
+#
+# main_dir = ".\\Day "
+#
+# for i in range(1, 101):
+#     os.mkdir(main_dir + str(i))
+#     f = open(main_dir + str(i) + "\\Readme.md", "x")
+#     f.write("Day {} project:".format(i))
+#     print("Directory '% s' is built!" % i)
